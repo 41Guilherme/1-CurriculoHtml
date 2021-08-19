dba91052b8c73f02c1543969f129f2ca770273d0
Guilherme José 41 <86620455+41Guilherme@users.noreply.github.com>
2021-08-18 22:10:48 -0300

    Second Version

---
+++ Curriculo/script.js
@@ -0,0 +1,19 @@
+/*
+var idade = prompt("Qual sua idade");
+
+if (idade >= 18) {
+    alert("Maior de Idade");
+}else{
+    alert("Menor de Idade");
+    close();
+}
+
+function soma(n1,n2) {
+    return n1 + n2;
+}*/
+
+function butao() {
+    document.getElementById("AAA").innerHTML = "Me contrate por favor kk";
+    
+    
+}
